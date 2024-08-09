0728c361ab333c000c2a16f3859e67c157c1e307
Alberto Cetoli <albertocetoli@gmail.com>
2024-08-09 18:43:22 +0100

    added config handler

--- wafl_llm/configuration.py
+++ wafl_llm/configuration.py
@@ -4,4 +4,5 @@ configuration_names_dict = {
     "whisper_model": "whisper=whisper.mar",
     "sentence_embedder_models": "sentence_embedder=sentence_embedder.mar",
     "entailer_model": "entailer=entailer.mar",
+    "configuration_model": "configuration=configuration.mar",
 }
